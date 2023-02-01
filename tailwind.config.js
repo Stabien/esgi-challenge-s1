e36ca43017e0bc9a227637c0ec208e213e65576a
RSelwa <selwa.raphael@gmail.com>
2023-02-01 14:47:01 +0100

    ✨ Tailwind installed

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./assets/**/*.js", "./templates/**/*.html.twig"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
