efa10dc1a3077fe401359c98b0bf51e0a1596a8f
BASTIEN PIEDALLU <bastien.piedallu@gmail.com>
2023-02-08 01:39:42 +0100

    add migration

---
+++ migrations/Version20230208003808.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230208003808 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE matchs ADD date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE matchs DROP date');
+    }
+}
--- src/Controller/Back/BetController.php
+++ src/Controller/Back/BetController.php
@@ -9,12 +9,12 @@ use App\Repository\BetRepository;
 use App\Entity\Bet;
 
 #[Route('/bet', name: 'bet_')]
-class BetsController extends AbstractController
+class BetController extends AbstractController
 {
     #[Route('/', name: 'index', methods: ['GET'])]
     public function index(BetRepository $betRepository): Response
     {
-        return $this->render('back/bets/index.html.twig', [
+        return $this->render('back/bet/index.html.twig', [
             'bets' => $betRepository->findBy([], ['date' => 'ASC']),
         ]);
     }
