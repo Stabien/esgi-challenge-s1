3e9264f40d71862fde4b01909af68c0ca53207d8
RSelwa <selwa.raphael@gmail.com>
2023-02-19 11:30:36 +0100

    💄 style login et redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./assets/**/*.js", "./templates/**/*.html.twig"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          50: "#ccf9f9",
+          150: "#0ac7b8",
+        },
+        gold: {
+          50: "#efe5d1",
+          150: "#c79a3c",
+        },
+        darkBlue: {
+          50: "#010a13",
+          150: "#0D1626",
+        },
+      },
+      fontFamily: {
+        beaufort: ["beaufort"],
+      },
+    },
   },
   plugins: [],
 };
