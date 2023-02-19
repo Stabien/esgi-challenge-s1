fc3ca124faabfcdad41f781a2f90deff8713ef12
RSelwa <selwa.raphael@gmail.com>
2023-02-19 14:01:42 +0100

    💄 card design

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,10 @@ module.exports = {
           50: "#010a13",
           150: "#0D1626",
         },
+        gradient: {
+          150: "#15395E",
+          250: "#AA2A16",
+        },
       },
       fontFamily: {
         beaufort: ["beaufort"],
