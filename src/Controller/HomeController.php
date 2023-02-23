bc55c30381ae08f2f96ba25ade844c3262af4378
BASTIEN PIEDALLU <bastien.piedallu@gmail.com>
2023-02-23 19:41:50 +0100

    fix: import match entity

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Matchs;
 
 class HomeController extends AbstractController
 {
