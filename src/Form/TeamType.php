abd37fc061d6ce37c33f608c1ce4d2e7943d1583
Mevin Redureau <mel.redureau@gmail.com>
2023-02-26 00:55:22 +0100

    Add css on all the Teams pages from admin POV

--- src/Form/TeamType.php
+++ src/Form/TeamType.php
@@ -4,17 +4,25 @@ namespace App\Form;
 
 use App\Entity\Team;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class TeamType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('logo')
-            ->add('region')
+            ->add('name', TextType::class, [
+                'attr' => ['class' => 'mb-5', 'placeholder' => 'Team Name']
+            ])
+            ->add('logo', TextType::class, [
+                'attr' => ['class' => 'mb-5', 'placeholder' => 'Nom logo sans -logo.png']
+            ])
+            ->add('region', TextType::class, [
+                'attr' => ['class' => 'mb-5', 'placeholder' => 'Nom région']
+            ])
         ;
     }
 
